33cd6d24c7085d161512320d8770d148c36bc15c
gevzib <gevzib-wensub-jarzu3@proton.me>
2024-01-25 16:20:06 +0000

    sorry for the rubbish message

---
+++ addlisting.js
@@ -0,0 +1,27 @@
+function add1() {
+    document.getElementById('list1').innerHTML += '<li><input id="time" type="time" name ="date1time" /></li>';
+}
+
+function add2() {
+    document.getElementById('list2').innerHTML += '<li><input id="time" type="time" name ="date2time" /></li>';
+}
+
+function add3() {
+    document.getElementById('list3').innerHTML += '<li><input id="time" type="time" name ="date3time" /></li>';
+}
+
+function add4() {
+    document.getElementById('list4').innerHTML += '<li><input id="time" type="time" name ="date4time" /></li>';
+}
+
+function add5() {
+    document.getElementById('list5').innerHTML += '<li><input id="time" type="time" name ="date5time" /></li>';
+}
+
+function add6() {
+    document.getElementById('list6').innerHTML += '<li><input id="time" type="time" name ="date6time" /></li>';
+}
+
+function add7() {
+    document.getElementById('list7').innerHTML += '<li><input id="time" type="time" name ="date7time" /></li>';
+}
