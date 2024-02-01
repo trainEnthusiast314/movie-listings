dbffaba3a72daf459745b5355f7bcf056ead96e1
gevzib <gevzib-wensub-jarzu3@proton.me>
2024-02-01 16:23:47 +0000

    some more updated to the addlistings page including session storage

--- addlisting.js
+++ addlisting.js
@@ -1,17 +1,5 @@
-function add(item) {
-    let node = document.createElement("li");
-    node.innerHTML = `<input id="time" type="time" name ="date1time" />`;
-    document.getElementById(item).appendChild(node);
-}
-
-
-
-
-
-
-
-let data = {
-
+if(data == undefined) {
+    data = {};
 }
 
 let dataBind = function() {
@@ -26,16 +14,6 @@ dataBind.set = function(key, value) {
     data[key] = value;
 }
 
-/*dataBind.display = function() {
-    document.querySelectorAll('[name], [data]').forEach((item)=>{
-        if(item.value != "" && item.value != undefined) {
-            console.log(item.name, item.value);
-            let key = item.getAttribute("data");
-            item.innerText = data[key];
-        } 
-    })
-}*/
-
 dataBind.display = function() {
     document.querySelectorAll('[name], [data]').forEach((item)=>{
         if(item.name == undefined) {
@@ -45,24 +23,15 @@ dataBind.display = function() {
     })
 }
 
-window.addEventListener("load", function() {
+dataBind.save = function() {
+    console.log(JSON.stringify(data));
+    sessionStorage.setItem('data', JSON.stringify(data));
+}
+ 
+dataBind.display();
 
-    document.getElementById("date1").value = "2024-02-02";
-    document.getElementById("date2").value = "2024-02-03";
-    document.getElementById("date3").value = "2024-02-04";
-    document.getElementById("date4").value = "2024-02-05";
-    document.getElementById("date5").value = "2024-02-06";
-    document.getElementById("date6").value = "2024-02-07";
-    document.getElementById("date7").value = "2024-02-08";
-
-    this.document.querySelector('form').addEventListener("input", function(e)
-    {
-        dataBind.set(e.target.name, e.target.value);
-        dataBind.display()
-    })
-
-    
+document.addEventListener("input", function(e) {
+    dataBind.set(e.target.name, e.target.value);
+    dataBind.display();
 })
-
-
 
